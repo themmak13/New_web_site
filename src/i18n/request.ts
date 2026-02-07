3d62d6219343628beae5b1486903c6d0c4aa48d2
themmak13 <68853379+themmak13@users.noreply.github.com>
2026-02-07 15:12:33 +0300

    Update request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,12 +1,15 @@
 import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
 
+// Valid locales
 const locales = ['en', 'ar'];
 
 export default getRequestConfig(async ({locale}) => {
   if (!locales.includes(locale as any)) notFound();
 
   return {
-    messages: (await import(`../messages/${locale}.json`)).default
+    // This assumes your messages are in root/messages/en.json
+    // Note the `../../` because we are now deeper in src/i18n/
+    messages: (await import(`../../messages/${locale}.json`)).default
   };
 });
